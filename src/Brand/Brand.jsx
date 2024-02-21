6032ccde9a3056ef3c2317b43be5e8c75dfa3e40
toaha77 <istiaktoaha07@gmail.com>
2024-02-21 21:06:16 +0600

    data added

--- src/Brand/Brand.jsx
+++ src/Brand/Brand.jsx
@@ -1,15 +1,26 @@
-import { useLoaderData } from "react-router-dom";
+ import { useEffect, useState } from "react";
 import BrandCard from "./BrandCard";
-
+ 
  
 const Brand = () => {
-    const brands = useLoaderData()
-    console.log(brands);
-     return (
+    const [product, setProduct] = useState([])
+
+    useEffect(()=>{
+      fetch('http://localhost:5000/product')
+      .then(res => res.json())
+      .then(data => setProduct(data))
+    }, [])
+      return (
         <div className="mt-4">
             <h2 className="text-5xl font-bold text-center text-blue-500 mb-4"> Featured Listings</h2>
-            <BrandCard brands={brands}></BrandCard>
-        </div>
+            <div className="flex items-center justify-center container mx-auto gap-4">
+        {
+          product.map(cloth=> <BrandCard cloth={cloth}
+          key={cloth._id}
+          ></BrandCard>)
+        }
+      </div>
+         </div>
     );
 };
 
--- src/Brand/BrandCard.jsx
+++ src/Brand/BrandCard.jsx
@@ -1,35 +1,9 @@
-import { Link } from "react-router-dom";
-import { WiDirectionRight} from "react-icons/wi"
- const BrandCard = ({ brands }) => {
- console.log(brands);
-    return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-      {brands?.map((brand) => {
-        return (
-          <div
-            key={brand.id}
-            className="card card-compact w-96 bg-base-100 shadow-xl hover:shadow-2xl"
-          >
-            <figure>
-              <img
-                src=
-            {brand.img}
-            className="h-[260px]"
-                alt="{brand.title}"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="text-2xl text-center">{brand.title}</h2>
-              
-              <div className="card-actions justify-center">
-               <Link to={`/brandsCard/${brand.title}`}>
-               <button  className="btn bg-blue-500 text-white hover:bg-blue-500 border">See Details <WiDirectionRight className="text-3xl"></WiDirectionRight> </button>
-               </Link>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+   const BrandCard = ({ cloth }) => {
+   const {_id, image} = cloth
+  return (
+    <div className="flex gap-4">
+
+      <img className="w-36" src={image} alt="" />
     </div>
   );
 };
